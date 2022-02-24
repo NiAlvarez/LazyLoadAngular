3f43cb4d0c8f32a07032fdadd16a13edc0d37cfb
NiAlvarez <nicolas.alvarez@fluxit.com.ar>
2022-02-24 08:57:43 -0300

    se sube progreso de app

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,21 @@
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule    
   ]
 })
 export class MaterialModule { }
